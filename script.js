5fa1a40a386ff38c10ead57f85abdb0b3aac5137
mond <natnatee_mond@hotmail.com>
2024-05-13 21:23:11 +0700

    v1

---
+++ script.js
@@ -0,0 +1,20 @@
+// let dayNumber = 8
+// if(dayNumber===1){
+//     console.log('Sunday')
+// }else if(dayNumber===2){
+//     console.log('Monday')
+// }else if(dayNumber===3){
+//     console.log('Tuesday')
+// }else if(dayNumber===4){
+//     console.log('Wednesday')
+// }else if(dayNumber===5){
+//     console.log('Thursday')
+// }else if(dayNumber===6){
+//     console.log('Friday')
+// }else if(dayNumber===7){
+//     console.log('Saturday')
+// }else {
+//     console.log('Invalid day number')
+// }
+let dayNumber = 8
+dayNumber = dayNumber===1?console.log('Sunday'):dayNumber===2?console.log('Monday'):dayNumber===3?console.log('tuesday'):dayNumber===4?console.log('Wednesday'):dayNumber===5?console.log('Thursday'):dayNumber===6?console.log('Friday'):dayNumber===7?console.log('Saturday'):console.log('Invalid day number');
